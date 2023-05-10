d56dbd73b64cf94d3f3d6984cee1f0a52ef5f7aa
umesh <umesharma.us11@gmail.com>
2023-05-10 21:13:08 +0530

    Deploying the production build

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-app.use(cors());
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method);
   console.log('Path: ', request.path);
@@ -13,8 +12,11 @@ const requestLogger = (request, response, next) => {
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
 };
+
 app.use(express.json());
 app.use(requestLogger);
+app.use(cors());
+app.use(express.static('build'));
 
 let notes = [
   {
