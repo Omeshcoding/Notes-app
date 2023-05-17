5df422fa5b92b598899147912fc14228f39f06e2
umesh <umesharma.us11@gmail.com>
2023-05-17 22:19:28 +0530

    Refactoring code for the post req and single note req

--- backend/index.js
+++ backend/index.js
@@ -51,16 +51,9 @@ app.get('/api/notes', (request, response) => {
 });
 // Get single note
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => {
-    return note.id === id;
-  });
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note);
-  } else {
-    response.statusMessage = 'Current note is not found Error 404';
-    response.status(404).end();
-  }
+  });
 });
 // Delete
 app.delete('/api/notes/:id', (request, response) => {
@@ -68,11 +61,6 @@ app.delete('/api/notes/:id', (request, response) => {
   notes = notes.filter((note) => note.id !== id);
   response.status(204).end();
 });
-// new Id
-const generatedId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
 
 // Post
 app.post('/api/notes', (request, response) => {
@@ -82,14 +70,14 @@ app.post('/api/notes', (request, response) => {
       error: 'content missing',
     });
   }
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generatedId(),
-  };
+  });
 
-  notes = notes.concat(note);
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 app.use(unknownEndpoint);
