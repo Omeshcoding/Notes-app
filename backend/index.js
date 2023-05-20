9b0df7be6652f9b487214ccd9713f6bebb6e2919
umesh <umesharma.us11@gmail.com>
2023-05-20 09:55:38 +0530

    Added Error handling to the app, and created update functionality

--- backend/index.js
+++ backend/index.js
@@ -12,13 +12,23 @@ const requestLogger = (request, response, next) => {
   console.log('---');
   next();
 };
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
 };
 
+// Error Handling
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+  if (error.name === 'castError') {
+    return response.status(400).send({ error: 'malformatting id' });
+  }
+  next(error);
+};
+
+app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
-app.use(cors());
 app.use(express.static('build'));
 
 let notes = [
@@ -38,10 +48,6 @@ let notes = [
     important: true,
   },
 ];
-// Get  root route
-app.get('/', (request, response) => {
-  response.send('<h1>Hello Awesome World</h1>');
-});
 
 // Get all notes
 app.get('/api/notes', (request, response) => {
@@ -50,16 +56,39 @@ app.get('/api/notes', (request, response) => {
   });
 });
 // Get single note
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
+app.get('/api/notes/:id', (error, request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 // Delete
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-  response.status(204).end();
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+// Update
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(req.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
 });
 
 // Post
@@ -81,6 +110,7 @@ app.post('/api/notes', (request, response) => {
 });
 
 app.use(unknownEndpoint);
+app.use(errorHandler);
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
