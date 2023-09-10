c1320821aa6fcceaafc1cde489c99cb334fc3f88
umesh <umesharma.us11@gmail.com>
2023-09-10 15:17:34 +0530

    Create function to limit creating notes to only logged in users

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -1,13 +1,23 @@
 const notesRouter = require('express').Router();
-
+const jwt = require('jsonwebtoken');
 const Note = require('../models/note');
 const User = require('../models/user');
+
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '');
+  }
+  return null;
+};
 
 // Get all notes
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({}).populate('user', { username: 1, name: 1 });
   response.json(notes);
 });
+
 // Get single note
 notesRouter.get('/:id', async (request, response) => {
   const note = await Note.findById(request.params.id);
@@ -21,8 +31,12 @@ notesRouter.get('/:id', async (request, response) => {
 // Post
 notesRouter.post('/', async (request, response) => {
   const body = request.body;
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' });
+  }
 
-  const user = await User.findById(body.userId);
+  const user = await User.findById(decodedToken.id);
 
   const note = new Note({
     content: body.content,
