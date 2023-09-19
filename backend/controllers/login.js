537fa9ee237d3c6f14d081c8ed670958c205dcac
umesh <umesharma.us11@gmail.com>
2023-09-19 21:55:59 +0530

    Added login functionality in frontend to the note app

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -6,24 +6,20 @@ const User = require('../models/user');
 loginRouter.post('/', async (request, response) => {
   const { username, password } = request.body;
   const user = await User.findOne({ username });
-  console.log(username, password, user.username);
   const passwordCorrect =
     user === null ? false : await bcrypt.compare(password, user.passwordHash);
-
   if (!(user && passwordCorrect)) {
     return response.status(401).json({
       error: 'invalid username or password',
     });
   }
-
   const userForToken = {
     username: user.username,
     id: user._id,
   };
-
-  //token expires in 60*60 seconds, that is, in one hour
+  // //token expires in 60*60 seconds, that is, in one hour
   const token = jwt.sign(userForToken, process.env.SECRET, {
-    epiresIn: 60 * 60,
+    expiresIn: 60 * 60,
   });
 
   response
--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,29 +1,18 @@
 import { useState, useEffect } from 'react';
+import loginService from './services/login';
 import Note from './components/Note';
 import noteService from './services/notes';
 import Notification from './components/Notification';
+import Footer from './components/Footer';
 
-const Footer = () => {
-  const footerStyle = {
-    color: 'green',
-    fontStyle: 'italic',
-    fontSize: 16,
-  };
-  return (
-    <div style={footerStyle}>
-      <br />
-      <em>
-        Note app, Department of Computer Science, University of Helsinki 2023
-      </em>
-    </div>
-  );
-};
-
-const App = (props) => {
+const App = () => {
   const [notes, setNotes] = useState(null);
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
@@ -50,6 +39,31 @@ const App = (props) => {
     });
   };
 
+  // handle Login
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      window.localStorage.setItem('loggedNoteappUser', JSON.stringify(user));
+      noteService.setToken(user.token);
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (exception) {
+      setErrorMessage('Wrong credentials');
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+  console.log(window.localStorage.removeItem('loggedNoteappUser'));
+  const loginForm = () => {};
+  const noteForm = () => {};
+
   // Toggle importance
   const toggleImportanceOf = (id) => {
     const note = notes.find((n) => n.id === id);
@@ -73,14 +87,53 @@ const App = (props) => {
         setNotes(notes.filter((n) => n.id !== id));
       });
   };
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser');
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      noteService.setToken(user.token);
+    }
+  }, []);
   if (!notes) {
     return null;
   }
-  // console.log(errorMessage);
+
   return (
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
+
+      {user && loginForm()}
+      {user && (
+        <div>
+          <p>{user.name} logged in</p>
+          {noteForm()}
+        </div>
+      )}
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
