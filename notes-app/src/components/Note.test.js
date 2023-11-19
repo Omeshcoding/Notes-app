23e38546bb52de43c7291c78a294040490ec032a
umesh <umesharma.us11@gmail.com>
2023-11-19 11:05:16 +0530

    Refactored the tests in the note component and made it successfull

--- notes-app/src/components/Note.test.js
+++ notes-app/src/components/Note.test.js
@@ -8,16 +8,62 @@ import { render, screen } from '@testing-library/react';
 import Note from './Note';
 import userEvent from '@testing-library/user-event';
 
-test('renders content', () => {
+test('does not render this', () => {
   const note = {
     content: 'This is a reminder',
-    importance: true,
+    important: true,
   };
+
   render(<Note note={note} />);
-  const element = screen.queryByText('do not want this thing to be rendered', {
+
+  const element = screen.queryByText('do not want this thing to be rendered');
+  expect(element).toBeNull();
+});
+
+test('renders content', () => {
+  const note = {
+    content: 'Does not work anymore :(',
+    important: true,
+  };
+
+  render(<Note note={note} />);
+
+  const element = screen.getByText('Does not work anymore :(', {
     exact: false,
   });
-  expect(element).toBeNull();
+
+  expect(element).toBeDefined();
+});
+
+test('renders content', () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true,
+  };
+
+  const { container } = render(<Note note={note} />);
+  const div = container.querySelector('.note');
+  screen.debug();
+
+  expect(div).toHaveTextContent(
+    'Component testing is done with react-testing-library'
+  );
+});
+
+test('renders content', () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true,
+  };
+
+  render(<Note note={note} />);
+
+  const element = screen.getByText(
+    'Component testing is done with react-testing-library',
+    { exact: false }
+  );
+
+  expect(element).toBeDefined();
 });
 
 test('clicking the button calls event handler once', async () => {
@@ -27,6 +73,7 @@ test('clicking the button calls event handler once', async () => {
   };
   const mockHandler = jest.fn();
   render(<Note note={note} toggleImportance={mockHandler} />);
+
   const user = userEvent.setup();
   const button = screen.getByText('make not important');
   await user.click(button);
